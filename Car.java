de653ebfe797bcbf0dbf91e6d4d933ade9460122
naiveCode <mayuresh.sri@gmail.com>
2016-06-22 06:29:11 +0530

    Initial setup

---
+++ Car.java
@@ -0,0 +1,13 @@
+package test.mayuresh;
+
+/**
+ * Created by naiveCode on 22/6/16.
+ */
+public class Car {
+    String registrationNo;
+    String color;
+    Car(String r, String c){
+        registrationNo = r;
+        color = c;
+    }
+}
